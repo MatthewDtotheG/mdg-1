3d816cc994fe36e3f294122081f58a133372e426
MatthewDtotheG <mdgarrett1984@gmail.com>
2019-07-23 19:02:11 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    clicked: false
+  };
+
+  handleClick = () => {
+    this.setState({
+      clicked: !this.state.clicked
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <title>MG</title>
+        </header>
+        {this.state.clicked === false ? (
+          <div>
+            <h1>Matthew Garrett</h1>
+            <h4>Full Stack Engineer</h4>
+            <ul>
+              <a href="https://github.com/MatthewDtotheG" target="_blank">
+                github
+              </a>
+              <a href="https://twitter.com/matthewdtotheg" target="_blank">
+                twitter
+              </a>
+              <a
+                href="https://www.linkedin.com/in/matthewdtotheg/"
+                target="_blank"
+              >
+                linkedin
+              </a>
+            </ul>
+            <div className="question" onClick={() => this.handleClick()}>
+              <a>?</a>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <p>
+              This site was built with React.js and just a pinch of Sass
+              <br />
+              Hosted on Netlify
+            </p>
+            <p />
+            <div className="close" onClick={() => this.handleClick()}>
+              <a>X</a>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
