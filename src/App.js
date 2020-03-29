d4b8ea501468937b2e4ac2c315814bbd32e4cd50
MatthewDtotheG <mdgarrett1984@gmail.com>
2020-03-29 16:17:31 -0400

    Adds blotter

--- src/App.js
+++ src/App.js
@@ -1,49 +1,70 @@
-import React, { useState } from "react";
+import React from "react";
+import { FliesText } from "react-text-fun";
 import "./App.scss";
 
- const App = () => {
-  const [secondPage, showPage] = useState('hide');
-    return (
-      <div className="App">
-        <header>
-          <title>MG</title>
-        </header>
-        {secondPage === 'hide' ? (
-          <div>
-            <h1>Matthew Garrett</h1>
-            <ul>
-              <a href="https://github.com/MatthewDtotheG" target="_blank">
-                <img src="./github.svg"/>
-              </a>
-              <a href="https://twitter.com/matthewdtotheg" target="_blank">
-                <img src="./twitter.svg"/>
-              </a>
-              <a
-                href="https://www.linkedin.com/in/matthewdtotheg/"
-                target="_blank"
-              >
-              <img src="./linkedin.svg"/>
-              </a>
-            </ul>
-            <div className="question" onClick={() => showPage('show')}>
-              <a>?</a>
-            </div>
-          </div>
-        ) : (
-          <div>
-            <p>
-              This site was built with React.js and just a pinch of Sass
-              <br />
-              Hosted on Netlify
-            </p>
-            <p />
-            <div className="close" onClick={() => showPage('hide')}>
-              <a>X</a>
-            </div>
-          </div>
-        )}
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className="App">
+      <header>
+        <title />
+      </header>
+      <body>
+        <FliesText
+          id="name"
+          cellRadius={0.7}
+          text="Matthew Garrett"
+          fontSize={130}
+          cellWidth={0.03}
+          speed={2}
+          dodge={false}
+        />
+        <FliesText
+          id="title"
+          cellRadius={1}
+          text="Web Developer / Brooklynite"
+          fontSize={50}
+          cellWidth={0.03}
+          speed={2}
+          dodge={false}
+        />
+        <ul>
+          <a href="https://github.com/MatthewDtotheG" target="_blank">
+            <FliesText
+              id="github"
+              cellRadius={2}
+              text="Github"
+              fontSize={30}
+              cellWidth={0.03}
+              speed={2}
+              dodge={false}
+            />
+          </a>
+          <a href="https://www.linkedin.com/in/matthewdtotheg/" target="_blank">
+            <FliesText
+              id="linkedin"
+              cellRadius={2}
+              text="Linkedin"
+              fontSize={30}
+              cellWidth={0.03}
+              speed={2}
+              dodge={false}
+            />
+          </a>
+          <a href="https://twitter.com/matthewdtotheg" target="_blank">
+            <FliesText
+              id="twitter"
+              cellRadius={2}
+              text="Twitter"
+              fontSize={30}
+              cellWidth={0.03}
+              speed={2}
+              dodge={false}
+            />
+          </a>
+        </ul>
+      </body>
+    </div>
+  );
+};
 
 export default App;
