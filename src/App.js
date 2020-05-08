6df9cf24b906a1366efe040883643176c1be13a1
MatthewDtotheG <mdgarrett1984@gmail.com>
2020-05-08 16:28:09 -0400

    Adds mobile version mk1

--- src/App.js
+++ src/App.js
@@ -1,68 +1,92 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FliesText } from "react-text-fun";
 import "./App.scss";
 
+function useWindowSize() {
+  const isClient = typeof window === "object";
+
+  function getSize() {
+    return {
+      width: isClient ? window.innerWidth : undefined,
+    };
+  }
+
+  const [windowSize, setWindowSize] = useState(getSize);
+
+  useEffect(() => {
+    if (!isClient) {
+      return false;
+    }
+
+    function handleResize() {
+      setWindowSize(getSize());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount and unmount
+
+  return windowSize;
+}
+
 const App = () => {
+  const size = useWindowSize();
+
   return (
-    <div className="App">
-      <header>
-        <title />
-      </header>
-      <body>
-        <FliesText
-          id="name"
-          cellRadius={0.7}
-          text="Matthew Garrett"
-          fontSize={130}
-          cellWidth={0.03}
-          speed={2}
-          dodge={false}
-        />
-        <FliesText
-          id="title"
-          cellRadius={3}
-          text="Web Developer / Brooklynite"
-          fontSize={50}
-          cellWidth={0.03}
-          speed={2}
-          dodge={false}
-        />
-        <ul>
-          <a href="https://github.com/MatthewDtotheG" target="_blank">
-            <FliesText
-              id="github"
-              cellRadius={3}
-              text="Github"
-              fontSize={50}
-              cellWidth={0.03}
-              speed={2}
-              dodge={false}
-            />
-          </a>
-          <a href="https://www.linkedin.com/in/matthewdtotheg/" target="_blank">
-            <FliesText
-              id="linkedin"
-              cellRadius={3}
-              text="Linkedin"
-              fontSize={50}
-              cellWidth={0.03}
-              speed={2}
-              dodge={false}
-            />
-          </a>
-          <a href="https://twitter.com/matthewdtotheg" target="_blank">
-            <FliesText
-              id="twitter"
-              cellRadius={3}
-              text="Twitter"
-              fontSize={50}
-              cellWidth={0.03}
-              speed={2}
-              dodge={false}
-            />
-          </a>
-        </ul>
-      </body>
+    <div className="container-wrapper">
+      <FliesText
+        id="name"
+        cellRadius={size.width < 913 ? 3 : 0.7}
+        text="Matthew Garrett"
+        fontSize={size.width < 913 ? 50 : 130}
+        cellWidth={0.03}
+        speed={2}
+        dodge={false}
+      />
+      <FliesText
+        id="title"
+        cellRadius={3}
+        text="Web Developer / Brooklynite"
+        fontSize={size.width < 913 ? 30 : 50}
+        cellWidth={0.03}
+        speed={2}
+        dodge={false}
+      />
+      <div className="link-wrapper">
+        <a href="https://github.com/MatthewDtotheG" target="_blank">
+          <FliesText
+            id="github"
+            cellRadius={3}
+            text="Github"
+            fontSize={size.width < 913 ? 30 : 50}
+            cellWidth={0.03}
+            speed={2}
+            dodge={false}
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/matthewdtotheg/" target="_blank">
+          <FliesText
+            id="linkedin"
+            cellRadius={3}
+            text="Linkedin"
+            fontSize={size.width < 913 ? 30 : 50}
+            cellWidth={0.03}
+            speed={2}
+            dodge={false}
+          />
+        </a>
+        <a href="https://twitter.com/matthewdtotheg" target="_blank">
+          <FliesText
+            id="twitter"
+            cellRadius={3}
+            text="Twitter"
+            fontSize={size.width < 913 ? 30 : 50}
+            cellWidth={0.03}
+            speed={2}
+            dodge={false}
+          />
+        </a>
+      </div>
     </div>
   );
 };
